1d24ebc50ace281edfe25717c2050f53392c49ff
iuliamariagroza <162835654+iuliamariagroza@users.noreply.github.com>
2025-03-24 11:19:34 +0200

    Update Lab3ex1.m

--- Semester 6/Numerical Calculus/Labs/Lab3/Lab3ex1.m
+++ Semester 6/Numerical Calculus/Labs/Lab3/Lab3ex1.m
@@ -1,1 +1,16 @@
+%ex1a
+x=[0,1,2];
+f=1./(1+x);
+t=divdiff(x,f);
+
+%ex1b
+df=-1./(1+x).^2;
+[z,t2]=divdiff2(x,f,df);
+
+%ex1c
+x=linspace(1,2,11);
+f=1./(1+x);
+df=-1./(1+x).^2;
+t3=divdiff(x,f)
+[z,t4]=divdiff2(x,f,df)
 
