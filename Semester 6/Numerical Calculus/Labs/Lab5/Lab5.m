a57394bb20f6d8aceb3b63a0978fce76da74b52d
iuliamariagroza <162835654+iuliamariagroza@users.noreply.github.com>
2025-03-25 16:53:54 +0200

    Update Lab5.m

--- Semester 6/Numerical Calculus/Labs/Lab5/Lab5.m
+++ Semester 6/Numerical Calculus/Labs/Lab5/Lab5.m
@@ -17,3 +17,21 @@ x0=zeros(size(b));
 err=10^(-5);
 
 [x,nit]=gaussSeidel(A,b,x0,err,maxnit);
+
+%ex2,a
+A = [10, 7, 8, 7; 7, 5, 6, 5; 8, 6, 10, 9; 7, 5, 9, 10];
+b = [32, 23, 33, 31]';
+x=inv(A)*b;
+
+%ex2,b
+b1=[32.1,22.9,33.1,30.9]';
+x1=inv(A)*b1;
+input_error=norm(b-b1,inf)/norm(b,inf);
+output_error=norm(x-x1,inf)/norm(x,inf);
+
+
+%ex2,c
+A1 = [10, 7, 8.1, 7.2; 7.8, 5.04, 6, 5; 8, 5.98, 9.89, 9; 6.99, 4.99, 9, 9.98];
+x2=inv(A1)*b
+input_errorc=norm(A-A1,inf)/norm(A,inf)
+output_errorc=norm(x-x2,inf)/norm(x,inf)
