04b0d4ca75169d21d97a60acbdf549889f7f4712
iuliamariagroza <162835654+iuliamariagroza@users.noreply.github.com>
2025-03-25 17:06:52 +0200

    Update Lab5.m

--- Semester 6/Numerical Calculus/Labs/Lab5/Lab5.m
+++ Semester 6/Numerical Calculus/Labs/Lab5/Lab5.m
@@ -25,13 +25,16 @@ x=inv(A)*b;
 
 %ex2,b
 b1=[32.1,22.9,33.1,30.9]';
-x1=inv(A)*b1;
-input_error=norm(b-b1,inf)/norm(b,inf);
-output_error=norm(x-x1,inf)/norm(x,inf);
+x1=inv(A)*b1
+input_error=norm(b-b1)/norm(b)
+output_error=norm(x-x1)/norm(x)
 
 
 %ex2,c
 A1 = [10, 7, 8.1, 7.2; 7.8, 5.04, 6, 5; 8, 5.98, 9.89, 9; 6.99, 4.99, 9, 9.98];
 x2=inv(A1)*b
-input_errorc=norm(A-A1,inf)/norm(A,inf)
-output_errorc=norm(x-x2,inf)/norm(x,inf)
+input_errorc=norm(A-A1)/norm(A)
+output_errorc=norm(x-x2)/norm(x)
+
+%ex2,d
+condA=norm(A)*norm(inv(A))
